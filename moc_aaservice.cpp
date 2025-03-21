2ab711f3a8ebbc7a4e84379d16af0cd5321d6b2f
pablo <pduce@swissblock.net>
2025-03-21 13:14:50 +0100

    working

---
+++ moc_aaservice.cpp
@@ -0,0 +1,467 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'aaservice.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "src/aaservice.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'aaservice.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_AAService_t {
+    QByteArrayData data[44];
+    char stringdata0[540];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_AAService_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_AAService_t qt_meta_stringdata_AAService = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "AAService"
+QT_MOC_LITERAL(1, 10, 19), // "videoSurfaceChanged"
+QT_MOC_LITERAL(2, 30, 0), // ""
+QT_MOC_LITERAL(3, 31, 13), // "statusChanged"
+QT_MOC_LITERAL(4, 45, 12), // "videoResized"
+QT_MOC_LITERAL(5, 58, 13), // "outputResized"
+QT_MOC_LITERAL(6, 72, 16), // "inputModeChanged"
+QT_MOC_LITERAL(7, 89, 15), // "playbackStarted"
+QT_MOC_LITERAL(8, 105, 9), // "connected"
+QT_MOC_LITERAL(9, 115, 12), // "notification"
+QT_MOC_LITERAL(10, 128, 19), // "btConnectionRequest"
+QT_MOC_LITERAL(11, 148, 7), // "address"
+QT_MOC_LITERAL(12, 156, 4), // "init"
+QT_MOC_LITERAL(13, 161, 5), // "start"
+QT_MOC_LITERAL(14, 167, 4), // "stop"
+QT_MOC_LITERAL(15, 172, 14), // "checkForDevice"
+QT_MOC_LITERAL(16, 187, 9), // "mouseDown"
+QT_MOC_LITERAL(17, 197, 5), // "point"
+QT_MOC_LITERAL(18, 203, 9), // "mouseMove"
+QT_MOC_LITERAL(19, 213, 7), // "mouseUp"
+QT_MOC_LITERAL(20, 221, 8), // "keyEvent"
+QT_MOC_LITERAL(21, 230, 3), // "key"
+QT_MOC_LITERAL(22, 234, 15), // "rotateClockwise"
+QT_MOC_LITERAL(23, 250, 22), // "rotateCounterClockwise"
+QT_MOC_LITERAL(24, 273, 20), // "rotateFlickClockwise"
+QT_MOC_LITERAL(25, 294, 27), // "rotateFlickCounterClockwise"
+QT_MOC_LITERAL(26, 322, 9), // "dpadClick"
+QT_MOC_LITERAL(27, 332, 8), // "dpadBack"
+QT_MOC_LITERAL(28, 341, 6), // "dpadUp"
+QT_MOC_LITERAL(29, 348, 8), // "dpadDown"
+QT_MOC_LITERAL(30, 357, 8), // "dpadLeft"
+QT_MOC_LITERAL(31, 366, 9), // "dpadRight"
+QT_MOC_LITERAL(32, 376, 12), // "videoSurface"
+QT_MOC_LITERAL(33, 389, 22), // "QAbstractVideoSurface*"
+QT_MOC_LITERAL(34, 412, 6), // "status"
+QT_MOC_LITERAL(35, 419, 10), // "videoWidth"
+QT_MOC_LITERAL(36, 430, 11), // "videoHeight"
+QT_MOC_LITERAL(37, 442, 11), // "outputWidth"
+QT_MOC_LITERAL(38, 454, 12), // "outputHeight"
+QT_MOC_LITERAL(39, 467, 9), // "inputMode"
+QT_MOC_LITERAL(40, 477, 9), // "InputMode"
+QT_MOC_LITERAL(41, 487, 16), // "INPUT_MODE_TOUCH"
+QT_MOC_LITERAL(42, 504, 17), // "INPUT_MODE_ROTARY"
+QT_MOC_LITERAL(43, 522, 17) // "INPUT_MODE_HYBRID"
+
+    },
+    "AAService\0videoSurfaceChanged\0\0"
+    "statusChanged\0videoResized\0outputResized\0"
+    "inputModeChanged\0playbackStarted\0"
+    "connected\0notification\0btConnectionRequest\0"
+    "address\0init\0start\0stop\0checkForDevice\0"
+    "mouseDown\0point\0mouseMove\0mouseUp\0"
+    "keyEvent\0key\0rotateClockwise\0"
+    "rotateCounterClockwise\0rotateFlickClockwise\0"
+    "rotateFlickCounterClockwise\0dpadClick\0"
+    "dpadBack\0dpadUp\0dpadDown\0dpadLeft\0"
+    "dpadRight\0videoSurface\0QAbstractVideoSurface*\0"
+    "status\0videoWidth\0videoHeight\0outputWidth\0"
+    "outputHeight\0inputMode\0InputMode\0"
+    "INPUT_MODE_TOUCH\0INPUT_MODE_ROTARY\0"
+    "INPUT_MODE_HYBRID"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_AAService[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      26,   14, // methods
+       7,  182, // properties
+       1,  210, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       8,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  144,    2, 0x06 /* Public */,
+       3,    0,  145,    2, 0x06 /* Public */,
+       4,    0,  146,    2, 0x06 /* Public */,
+       5,    0,  147,    2, 0x06 /* Public */,
+       6,    0,  148,    2, 0x06 /* Public */,
+       7,    0,  149,    2, 0x06 /* Public */,
+       8,    1,  150,    2, 0x06 /* Public */,
+      10,    1,  153,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+      12,    0,  156,    2, 0x0a /* Public */,
+      13,    0,  157,    2, 0x0a /* Public */,
+      14,    0,  158,    2, 0x0a /* Public */,
+      15,    0,  159,    2, 0x08 /* Private */,
+
+ // methods: name, argc, parameters, tag, flags
+      16,    1,  160,    2, 0x02 /* Public */,
+      18,    1,  163,    2, 0x02 /* Public */,
+      19,    1,  166,    2, 0x02 /* Public */,
+      20,    1,  169,    2, 0x02 /* Public */,
+      22,    0,  172,    2, 0x02 /* Public */,
+      23,    0,  173,    2, 0x02 /* Public */,
+      24,    0,  174,    2, 0x02 /* Public */,
+      25,    0,  175,    2, 0x02 /* Public */,
+      26,    0,  176,    2, 0x02 /* Public */,
+      27,    0,  177,    2, 0x02 /* Public */,
+      28,    0,  178,    2, 0x02 /* Public */,
+      29,    0,  179,    2, 0x02 /* Public */,
+      30,    0,  180,    2, 0x02 /* Public */,
+      31,    0,  181,    2, 0x02 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QVariantMap,    9,
+    QMetaType::Void, QMetaType::QString,   11,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // methods: parameters
+    QMetaType::Bool, QMetaType::QPoint,   17,
+    QMetaType::Bool, QMetaType::QPoint,   17,
+    QMetaType::Bool, QMetaType::QPoint,   17,
+    QMetaType::Bool, QMetaType::QString,   21,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Bool,
+
+ // properties: name, type, flags
+      32, 0x80000000 | 33, 0x0049510b,
+      34, QMetaType::Int, 0x00495001,
+      35, QMetaType::Int, 0x00495001,
+      36, QMetaType::Int, 0x00495001,
+      37, QMetaType::Int, 0x00495003,
+      38, QMetaType::Int, 0x00495003,
+      39, QMetaType::Int, 0x00495103,
+
+ // properties: notify_signal_id
+       0,
+       1,
+       2,
+       2,
+       3,
+       3,
+       4,
+
+ // enums: name, alias, flags, count, data
+      40,   40, 0x0,    3,  215,
+
+ // enum data: key, value
+      41, uint(AAService::INPUT_MODE_TOUCH),
+      42, uint(AAService::INPUT_MODE_ROTARY),
+      43, uint(AAService::INPUT_MODE_HYBRID),
+
+       0        // eod
+};
+
+void AAService::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<AAService *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->videoSurfaceChanged(); break;
+        case 1: _t->statusChanged(); break;
+        case 2: _t->videoResized(); break;
+        case 3: _t->outputResized(); break;
+        case 4: _t->inputModeChanged(); break;
+        case 5: _t->playbackStarted(); break;
+        case 6: _t->connected((*reinterpret_cast< QVariantMap(*)>(_a[1]))); break;
+        case 7: _t->btConnectionRequest((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 8: _t->init(); break;
+        case 9: _t->start(); break;
+        case 10: _t->stop(); break;
+        case 11: _t->checkForDevice(); break;
+        case 12: { bool _r = _t->mouseDown((*reinterpret_cast< QPoint(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 13: { bool _r = _t->mouseMove((*reinterpret_cast< QPoint(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 14: { bool _r = _t->mouseUp((*reinterpret_cast< QPoint(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 15: { bool _r = _t->keyEvent((*reinterpret_cast< QString(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 16: { bool _r = _t->rotateClockwise();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 17: { bool _r = _t->rotateCounterClockwise();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 18: { bool _r = _t->rotateFlickClockwise();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 19: { bool _r = _t->rotateFlickCounterClockwise();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 20: { bool _r = _t->dpadClick();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 21: { bool _r = _t->dpadBack();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 22: { bool _r = _t->dpadUp();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 23: { bool _r = _t->dpadDown();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 24: { bool _r = _t->dpadLeft();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 25: { bool _r = _t->dpadRight();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (AAService::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AAService::videoSurfaceChanged)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (AAService::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AAService::statusChanged)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (AAService::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AAService::videoResized)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (AAService::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AAService::outputResized)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (AAService::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AAService::inputModeChanged)) {
+                *result = 4;
+                return;
+            }
+        }
+        {
+            using _t = void (AAService::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AAService::playbackStarted)) {
+                *result = 5;
+                return;
+            }
+        }
+        {
+            using _t = void (AAService::*)(QVariantMap );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AAService::connected)) {
+                *result = 6;
+                return;
+            }
+        }
+        {
+            using _t = void (AAService::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AAService::btConnectionRequest)) {
+                *result = 7;
+                return;
+            }
+        }
+    } else if (_c == QMetaObject::RegisterPropertyMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 0:
+            *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAbstractVideoSurface* >(); break;
+        }
+    }
+
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<AAService *>(_o);
+        (void)_t;
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< QAbstractVideoSurface**>(_v) = _t->videoSurface(); break;
+        case 1: *reinterpret_cast< int*>(_v) = _t->status(); break;
+        case 2: *reinterpret_cast< int*>(_v) = _t->videoWidth(); break;
+        case 3: *reinterpret_cast< int*>(_v) = _t->videoHeight(); break;
+        case 4: *reinterpret_cast< int*>(_v) = _t->m_outputWidth; break;
+        case 5: *reinterpret_cast< int*>(_v) = _t->m_outputHeight; break;
+        case 6: *reinterpret_cast< int*>(_v) = _t->inputMode(); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+        auto *_t = static_cast<AAService *>(_o);
+        (void)_t;
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: _t->setVideoSurface(*reinterpret_cast< QAbstractVideoSurface**>(_v)); break;
+        case 4:
+            if (_t->m_outputWidth != *reinterpret_cast< int*>(_v)) {
+                _t->m_outputWidth = *reinterpret_cast< int*>(_v);
+                Q_EMIT _t->outputResized();
+            }
+            break;
+        case 5:
+            if (_t->m_outputHeight != *reinterpret_cast< int*>(_v)) {
+                _t->m_outputHeight = *reinterpret_cast< int*>(_v);
+                Q_EMIT _t->outputResized();
+            }
+            break;
+        case 6: _t->setInputMode(*reinterpret_cast< int*>(_v)); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::ResetProperty) {
+    }
+#endif // QT_NO_PROPERTIES
+}
+
+QT_INIT_METAOBJECT const QMetaObject AAService::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_AAService.data,
+    qt_meta_data_AAService,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *AAService::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *AAService::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_AAService.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int AAService::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 26)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 26;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 26)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 26;
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 7;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 7;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 7;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 7;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
+        _id -= 7;
+    }
+#endif // QT_NO_PROPERTIES
+    return _id;
+}
+
+// SIGNAL 0
+void AAService::videoSurfaceChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void AAService::statusChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void AAService::videoResized()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+
+// SIGNAL 3
+void AAService::outputResized()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, nullptr);
+}
+
+// SIGNAL 4
+void AAService::inputModeChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 4, nullptr);
+}
+
+// SIGNAL 5
+void AAService::playbackStarted()
+{
+    QMetaObject::activate(this, &staticMetaObject, 5, nullptr);
+}
+
+// SIGNAL 6
+void AAService::connected(QVariantMap _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 6, _a);
+}
+
+// SIGNAL 7
+void AAService::btConnectionRequest(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 7, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
